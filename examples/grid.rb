483e79ab8c99b53edb0b5f72406a34f8e0ec246c
Andy Maleh <andy.am@gmail.com>
2021-09-22 10:54:32 -0400

    Improved examples/grid.rb

--- examples/grid.rb
+++ examples/grid.rb
@@ -4,47 +4,43 @@ require 'glimmer-dsl-libui'
 
 include Glimmer
 
-window('Grid', 400) {
-  tab {
-    tab_item('Horizontal Arrangement') {
-      grid {
-        label('One') {
-          left 0
-          top 0
-        }
-        label('Two') {
-          left 1
-          top 0
-        }
-        label('Three') {
-          left 2
-          top 0
-        }
-        label('Four') {
-          left 3
-          top 0
+window('Grid') {
+  grid {
+    4.times { |left_value|
+      4.times { |top_value|
+        label("(#{left_value}, #{top_value}) xspan1\nyspan1") {
+          left left_value
+          top top_value
+          hexpand true
+          vexpand true
         }
       }
     }
-    tab_item('Vertical Arrangement') {
-      grid {
-        label('One') {
-          left 0
-          top 0
-        }
-        label('Two') {
-          left 0
-          top 1
-        }
-        label('Three') {
-          left 0
-          top 2
-        }
-        label('Four') {
-          left 0
-          top 3
-        }
-      }
+    label("(0, 4) xspan2\nyspan1 more text fits horizontally") {
+      left 0
+      top 4
+      xspan 2
+    }
+    label("(2, 4) xspan2\nyspan1 more text fits horizontally") {
+      left 2
+      top 4
+      xspan 2
+    }
+    label("(0, 5) xspan1\nyspan2\nmore text\nfits vertically") {
+      left 0
+      top 5
+      yspan 2
+    }
+    label("(0, 7) xspan1\nyspan2\nmore text\nfits vertically") {
+      left 0
+      top 7
+      yspan 2
+    }
+    label("(1, 5) xspan3\nyspan4 a lot more text fits horizontally than before\nand\neven\na lot\nmore text\nfits vertically\nthan\nbefore") {
+      left 1
+      top 5
+      xspan 3
+      yspan 4
     }
   }
 }.show
