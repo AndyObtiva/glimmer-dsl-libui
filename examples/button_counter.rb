fb030c5aaff3b04640a965a8b83130b9d4bd6af6
Andy Maleh <andy.am@gmail.com>
2021-11-26 15:05:32 -0500

    examples/button_counter.rb (takes advantage of unidirectional data-binding)

---
+++ examples/button_counter.rb
@@ -0,0 +1,25 @@
+require 'glimmer-dsl-libui'
+
+class ButtonCounter
+  include Glimmer
+
+  attr_accessor :count
+
+  def initialize
+    @count = 0
+  end
+
+  def launch
+    window('Hello, Button!', 190, 10) {
+      button('Count') {
+        text <= [self, :count, on_read: ->(count) {"Count: #{count}"}] # data-bind button text to self count, converting to string on read.
+        
+        on_clicked do
+          self.count += 1
+        end
+      }
+    }.show
+  end
+end
+
+ButtonCounter.new.launch
