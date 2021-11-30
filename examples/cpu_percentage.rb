65897bea7db73a39275c689f71eecdb5b28010a8
Andy Maleh <andy.am@gmail.com>
2021-11-30 17:21:13 -0500

    WIP on CPU Percentage (works in Windows only)

---
+++ examples/cpu_percentage.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'glimmer-dsl-libui'
+
+include Glimmer
+
+data = [
+  ['CPU', '0%', 0],
+]
+
+Glimmer::LibUI.timer(1) do
+  cpu_percentage_raw_value = `wmic cpu get loadpercentage`
+  cpu_percentage_value = cpu_percentage_raw_value.split("\n")[2].to_i
+  data[0][1] = "#{cpu_percentage_value}%"
+  data[0][2] = cpu_percentage_value
+end
+
+window('CPU Percentage', 400, 200) {
+  vertical_box {
+    table {
+      text_column('Name')
+      text_column('Value')
+      progress_bar_column('Percentage')
+
+      cell_rows data # implicit data-binding
+    }
+  }
+}.show
