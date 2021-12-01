3d2eb8e8b776bf174a56debd8c798e37671a8b64
Andy Maleh <andy.am@gmail.com>
2021-11-30 20:58:41 -0500

    Add Mac support to examples/cpu_percentage.rb

--- examples/cpu_percentage.rb
+++ examples/cpu_percentage.rb
@@ -1,6 +1,5 @@
-# frozen_string_literal: true
-
 require 'glimmer-dsl-libui'
+require 'bigdecimal'
 
 include Glimmer
 
@@ -12,9 +11,13 @@ Glimmer::LibUI.timer(1) do
   cpu_percentage_value = nil
   if OS.windows?
     cpu_percentage_raw_value = `wmic cpu get loadpercentage`
-    cpu_percentage_value = cpu_percentage_raw_value.split("\n")[2].to_f
+    cpu_percentage_value = cpu_percentage_raw_value.split("\n")[2].to_i
   elsif OS.mac?
-    cpu_percentage_value = `ps -A -o %cpu | awk '{s+=$1} END {print s}'`
+    cpu_percentage_value = `ps -A -o %cpu | awk '{s+=$1} END {print s}'`.to_i
+  elsif OS.linux?
+    stats = `top -n 1`
+    idle_percentage = stats.split("\n")[2].match(/ni,.* (.*) .*id/)[1]
+    cpu_percentage_value = (BigDecimal(100) - BigDecimal(idle_percentage)).to_i
   end
   data[0][1] = "#{cpu_percentage_value}%"
   data[0][2] = cpu_percentage_value
