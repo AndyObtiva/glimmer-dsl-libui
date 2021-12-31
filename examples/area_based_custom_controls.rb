22d13945ef82c5980507752ad325884933862129
Andy Maleh <andy.am@gmail.com>
2021-12-31 13:22:09 -0500

    Custom button construction in examples/area_based_custom_controls.rb

--- examples/area_based_custom_controls.rb
+++ examples/area_based_custom_controls.rb
@@ -46,27 +46,62 @@ def push_button(button_text,
   }
 end
 
-window('Area-Based Custom Controls', 270, 270) {
-  vertical_box {
-    push_button('Push') {
-      on_mouse_up do
-        message_box('Button Pushed', 'Thank you for clicking the button')
-      end
+class AreaBasedCustomControls
+  include Glimmer
+  
+  attr_accessor :button_width, :button_height, :button_color, :button_font_size, :button_text_x, :button_text_y, :button_border_color
+  
+  def initialize
+    self.button_width = 80
+    self.button_height = 30
+    self.button_font_size = 12
+    self.button_color = :white
+    self.button_border_color = {r: 201, g: 201, b: 201}
+  end
+  
+  def rebuild_push_button
+    @push_button.destroy
+    @push_button_container.content { # re-open vertical box content and shove in a new button
+      @push_button = push_button('Push',
+                                 width: button_width, height: button_height,
+                                 color: button_color, border_color: button_border_color,
+                                 font_descriptor: {size: button_font_size}) {
+        on_mouse_up do
+          message_box('Button Pushed', 'Thank you for pushing the button')
+        end
+      }
     }
-    push_button('Push', width: 100, height: 40, color: :yellow, font_descriptor: {size: 16}) {
-      on_mouse_up do
-        message_box('Button Pushed', 'Thank you for clicking the button')
-      end
-    }
-    push_button('Push', width: 120, height: 50, color: :lightgreen, font_descriptor: {size: 20}) {
-      on_mouse_up do
-        message_box('Button Pushed', 'Thank you for clicking the button')
-      end
-    }
-    push_button('Push', width: 160, height: 60, color: :cyan, font_descriptor: {size: 24}, text_x: 54) {
-      on_mouse_up do
-        message_box('Button Pushed', 'Thank you for pushing the button')
-      end
-    }
-  }
-}.show
+  end
+  
+  def launch
+    window('Area-Based Custom Controls', 270, 270) {
+      vertical_box {
+        label('Push Button Construction Form') {
+          stretchy false
+        }
+        form {
+          color_button {
+            label 'Color'
+            color <=> [self, :button_color, after_write: method(:rebuild_push_button)]
+          }
+        }
+        horizontal_box {
+          label # filler to center button
+          @push_button_container = vertical_box {
+            @push_button = push_button('Push',
+                                       width: button_width, height: button_height,
+                                       color: button_color, border_color: button_border_color,
+                                       font_descriptor: {size: button_font_size}) {
+              on_mouse_up do
+                message_box('Button Pushed', 'Thank you for pushing the button')
+              end
+            }
+          }
+          label # filler to center button
+        }
+      }
+    }.show
+  end
+end
+
+AreaBasedCustomControls.new.launch
