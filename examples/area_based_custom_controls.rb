c6f40d87b77a2a04ceb26ae635523b006405556c
Andy Maleh <andy.am@gmail.com>
2021-12-31 12:49:16 -0500

    Flat push button in examples/area_based_custom_controls.rb

--- examples/area_based_custom_controls.rb
+++ examples/area_based_custom_controls.rb
@@ -2,59 +2,43 @@ require 'glimmer-dsl-libui'
 
 include Glimmer
 
-COLOR_GRAY = {r: 192, g: 192, b: 192}
-
-def push_button(button_text, button_width: 50, button_height: 30, button_font: {}, bevel_percentage: 16, bevel_constant: 20, button_color: :white, &content)
-  bevel_width_pixel_size = button_width.to_f * (bevel_percentage / 100.0)
-  bevel_height_pixel_size = button_width.to_f * (bevel_percentage / 100.0)
-  # gradient beginning: 72  146  247
-  # gradient end: 12  85  214
-  button_color = Glimmer::LibUI.interpret_color(button_color)
+# Graphical push button built with vector graphics on top of area
+# text_x and text_y are the offset of the button text in releation to its top-left corner
+# When text_x, text_y are left nil, the text is automatically centered in the button area.
+# Sometimes, the centering calculation is not perfect due to using a custom font, so
+# in that case, pass in text_x, and text_y manually
+def push_button(button_text,
+                width: 80, height: 30, font_descriptor: {}, color: :white,
+                text_x: nil, text_y: nil, border_color: {r: 201, g: 201, b: 201},
+                &content)
+  color = Glimmer::LibUI.interpret_color(color)
   button_parts = {}
   area { |a|
-    button_parts[:background_rectangle] = rectangle(0, 0, button_width, button_height) {
-      fill button_color
+    button_parts[:background_rectangle] = rectangle(1, 1, width, height) {
+      fill color
     }
     
-    button_parts[:top_bevel_edge] = polygon {
-      point_array 0, 0, button_width, 0, button_width - bevel_width_pixel_size, bevel_height_pixel_size, bevel_width_pixel_size, bevel_height_pixel_size
-      fill r: button_color[:r] + 4*bevel_constant, g: button_color[:g] + 4*bevel_constant, b: button_color[:b] + 4*bevel_constant
+    button_parts[:border_rectangle] = rectangle(1, 1, width, height) {
+      stroke border_color
     }
     
-    button_parts[:right_bevel_edge] = polygon {
-      point_array button_width, 0, button_width - bevel_width_pixel_size, bevel_height_pixel_size, button_width - bevel_width_pixel_size, button_height - bevel_height_pixel_size, button_width, button_height
-      fill r: button_color[:r] - bevel_constant, g: button_color[:g] - bevel_constant, b: button_color[:b] - bevel_constant
-    }
-    
-    button_parts[:bottom_bevel_edge] = polygon {
-      point_array button_width, button_height, 0, button_height, bevel_width_pixel_size, button_height - bevel_height_pixel_size, button_width - bevel_width_pixel_size, button_height - bevel_height_pixel_size
-      fill r: button_color[:r] - bevel_constant, g: button_color[:g] - bevel_constant, b: button_color[:b] - bevel_constant
-    }
-    
-    button_parts[:left_bevel_edge] = polygon {
-      point_array 0, 0, 0, button_height, bevel_width_pixel_size, button_height - bevel_height_pixel_size, bevel_width_pixel_size, bevel_height_pixel_size
-      fill r: button_color[:r] - bevel_constant, g: button_color[:g] - bevel_constant, b: button_color[:b] - bevel_constant
-    }
-    
-    button_parts[:border_rectangle] = rectangle(0, 0, button_width, button_height) {
-      stroke COLOR_GRAY
-    }
-    
-    text_font_size = (button_font[:size] || 12)
-    text_width = text_font_size * button_text.size
-    text(button_width / 2.0 - text_width, button_height / 2.0 - text_font_size, text_width) {
+    text_height = (font_descriptor[:size] || 12) * 0.75
+    text_width = (text_height * button_text.size) * 0.75
+    text_x ||= (width - text_width) / 2.0
+    text_y ||= (height - 4 - text_height) / 2.0
+    text(text_x, text_y, width) {
       button_parts[:button_string] = string(button_text) {
-        font button_font
+        font font_descriptor
       }
     }
     
     on_mouse_down do
-      button_parts[:background_rectangle].fill = {x0: 0, y0: 0, x1: 0, y1: button_height, stops: [{pos: 0, r: 72, g: 146, b: 247}, {pos: 1, r: 12, g: 85, b: 214}]}
+      button_parts[:background_rectangle].fill = {x0: 0, y0: 0, x1: 0, y1: height, stops: [{pos: 0, r: 72, g: 146, b: 247}, {pos: 1, r: 12, g: 85, b: 214}]}
       button_parts[:button_string].color = :white
     end
     
     on_mouse_up do
-      button_parts[:background_rectangle].fill = button_color
+      button_parts[:background_rectangle].fill = color
       button_parts[:button_string].color = :black
     end
     
@@ -62,21 +46,27 @@ def push_button(button_text, button_width: 50, button_height: 30, button_font: {
   }
 end
 
-window('Area-Based Custom Controls', 320, 240) {
+window('Area-Based Custom Controls', 270, 270) {
   vertical_box {
-    push_button('Push', button_color: :lightgray, button_width: 300, button_height: 200, button_font: {size: 30}, bevel_percentage: 4) {
+    push_button('Push') {
+      on_mouse_up do
+        message_box('Button Pushed', 'Thank you for clicking the button')
+      end
+    }
+    push_button('Push', width: 100, height: 40, color: :yellow, font_descriptor: {size: 16}) {
+      on_mouse_up do
+        message_box('Button Pushed', 'Thank you for clicking the button')
+      end
+    }
+    push_button('Push', width: 120, height: 50, color: :lightgreen, font_descriptor: {size: 20}) {
+      on_mouse_up do
+        message_box('Button Pushed', 'Thank you for clicking the button')
+      end
+    }
+    push_button('Push', width: 160, height: 60, color: :cyan, font_descriptor: {size: 24}, text_x: 54) {
       on_mouse_up do
         message_box('Button Pushed', 'Thank you for pushing the button')
       end
     }
-    
-    button('Push') {
-      stretchy false
-      
-      on_clicked do
-        message_box('Button Pushed', 'Thank you for pushing the button')
-      end
-    }
-    
   }
 }.show
