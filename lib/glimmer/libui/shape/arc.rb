52e918facc5628db88e92e5bc84aaeed233e4f5e
Andy Maleh <andy.am@gmail.com>
2022-02-15 21:29:40 -0500

    Handle arc negative value with contain?/include?

--- lib/glimmer/libui/shape/arc.rb
+++ lib/glimmer/libui/shape/arc.rb
@@ -50,8 +50,8 @@ module Glimmer
           if perfect_shape_dependencies != @perfect_shape_dependencies
             x_center, y_center, radius, start_angle, sweep, is_negative = @perfect_shape_dependencies = perfect_shape_dependencies
             sign = is_negative ? 1 : -1
-            start = start_angle*sign
-            extent = sweep*sign
+            start = is_negative ? (360 - start_angle) : -1*start_angle
+            extent = is_negative ? (360 - sweep) : -1*sweep
             @perfect_shape = PerfectShape::Arc.new(
               type: :open,
               center_x: x_center, center_y: y_center,
