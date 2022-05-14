4abb8e37676d034baf37d4208d6b0f90e6275d39
Andy Maleh <andy.am@gmail.com>
2022-05-14 11:20:39 -0400

    Add more shapes to ShapeColoring example

--- examples/shape_coloring.rb
+++ examples/shape_coloring.rb
@@ -7,21 +7,42 @@ class ShapeColoring
   
   body {
     window('Shape Coloring') {
-      margined true
+      margined false
 
       horizontal_box {
         area {
-          rectangle(0, 0, 20, 20) { |shape|
-            fill :red
-            
-            on_mouse_up do |area_mouse_event|
-              shape.fill = COLORS.sample
-            end
+          rectangle(0, 0, 400, 300) { # background shape
+            fill :white
+          }
+          colorable(:rectangle, 10, 10, 40, 20) { |shape|
+            fill COLORS.sample
+          }
+          colorable(:square, 70, 20, 20) { |shape|
+            fill COLORS.sample
+          }
+          colorable(:circle, 60, 60, 20, 20) { |shape|
+            fill COLORS.sample
+          }
+          colorable(:arc, 90, 70, 40, 0, 145) { |shape|
+            fill COLORS.sample
+          }
+          colorable(:polygon, 120, 10, 120, 50, 150, 10, 150, 50) {
+            fill COLORS.sample
           }
         }
       }
     }
   }
+  
+  def colorable(shape_symbol, *args, &content)
+    send(shape_symbol, *args) do |shape|
+      on_mouse_up do |area_mouse_event|
+        shape.fill = COLORS.sample
+      end
+      
+      content.call(shape)
+    end
+  end
 end
 
 ShapeColoring.launch
