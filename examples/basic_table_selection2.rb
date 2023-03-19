fcc762b23eaeace40eca248800f872861d5c275b
Andy Maleh <andy.am@gmail.com>
2023-03-19 17:17:59 -0400

    Add Zero-Or-One table to examples/basic_table_selection2.rb

--- examples/basic_table_selection2.rb
+++ examples/basic_table_selection2.rb
@@ -61,6 +61,13 @@ class BasicTableSelection
       selection: 2, # initial selection row index (could be nil too or just left off, defaulting to 0)
       header_visible: nil, # defaults to true
     )
+    @zero_or_one_table_presenter = TablePresenter.new(
+      data: data.dup,
+      column_names: ['Name', 'Description'],
+      selection_mode: :zero_or_one, # other values are :zero_or_many , :zero_or_one, :none (default is :zero_or_one if not specified)
+      selection: nil, # initial selection row index (could be an integer too or just left off, defaulting to nil)
+      header_visible: nil, # defaults to true
+    )
   end
   
   body {
@@ -101,6 +108,61 @@ class BasicTableSelection
               selection_mode <= [@one_table_presenter, :selection_mode]
               selection <=> [@one_table_presenter, :selection]
               header_visible <= [@one_table_presenter, :header_visible]
+        
+              on_row_clicked do |t, row|
+                puts "Row Clicked: #{row}"
+              end
+        
+              on_row_double_clicked do |t, row|
+                puts "Row Double Clicked: #{row}"
+              end
+        
+              on_selection_changed do |t, selection, added_selection, removed_selection|
+                # selection is an array or nil if selection mode is zero_or_many
+                # otherwise, selection is a single index integer or nil when not selected
+                puts "Selection Changed: #{selection.inspect}"
+                puts "Added Selection: #{added_selection.inspect}"
+                puts "Removed Selection: #{removed_selection.inspect}"
+              end
+            }
+          }
+        }
+        
+        tab_item('Zero-Or-One') {
+          vertical_box {
+            vertical_box {
+              stretchy false
+              
+              @zero_or_one_table_selection_radio_buttons = radio_buttons {
+                items @zero_or_one_table_presenter.selection_items
+                selected <=> [@zero_or_one_table_presenter, :selection]
+              }
+            }
+            
+            button('Toggle Table Header Visibility') {
+              stretchy false
+              
+              on_clicked do
+                @zero_or_one_table_presenter.toggle_header_visible
+              end
+            }
+            
+            @zero_or_one_table = table {
+              @zero_or_one_table_presenter.column_presenters.each do |column_presenter|
+                text_column(column_presenter.name) {
+                  sort_indicator <=> [column_presenter, :sort_indicator]
+                  
+                  on_clicked do |tc, column|
+                    puts "Clicked column #{column}: #{tc.name}"
+                    column_presenter.sort
+                  end
+                }
+              end
+        
+              cell_rows @zero_or_one_table_presenter.data
+              selection_mode <= [@zero_or_one_table_presenter, :selection_mode]
+              selection <=> [@zero_or_one_table_presenter, :selection]
+              header_visible <= [@zero_or_one_table_presenter, :header_visible]
         
               on_row_clicked do |t, row|
                 puts "Row Clicked: #{row}"
