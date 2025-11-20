6824670d51e2d590e6e94d167a0b6796af58f714
Andy Maleh <andy.am@gmail.com>
2025-11-20 14:44:16 -0500

    Refactor/simplify examples/button_counter.rb by removing unnecessary `vertical_box` (Glimmer DSL for LibUI implicitly adds it automatically when needed)

--- examples/button_counter.rb
+++ examples/button_counter.rb
@@ -17,16 +17,14 @@ class ButtonCounter
 
   body {
     window('Hello, Button!', 190, 20) {
-      vertical_box {
-        button {
-          # data-bind button text to @counter count, converting to string on read from model.
-          text <= [@counter, :count, on_read: ->(count) {"Count: #{count}"}]
-          
-          on_clicked do
-            # This change will automatically propagate to button text through data-binding above
-            @counter.count += 1
-          end
-        }
+      button {
+        # data-bind button text to @counter count, converting to string on read from model.
+        text <= [@counter, :count, on_read: ->(count) {"Count: #{count}"}]
+        
+        on_clicked do
+          # This count change will automatically propagate to button text through data-binding above
+          @counter.count += 1
+        end
       }
     }
   }
